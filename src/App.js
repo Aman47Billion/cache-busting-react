b2a57f3e8ab78c97b95e678c6180e31bf459404d
shashank47b <shashank.kanungo@47billion.com>
2021-07-16 14:24:36 +0530

    cache-improve

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h4>Welcome To React Cache Busting....</h4>
+      <h4>47 Billion Front End</h4>
       <CacheBusting />
     </div>
   );

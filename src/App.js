b57147d0fc111e50f80155f82f408af3bd496b42
shashank47b <shashank.kanungo@47billion.com>
2021-07-16 13:44:58 +0530

    cache check

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CacheBusting from "./Components/CacheBusting";
 function App() {
   return (
     <div className="App">
-      <h4>Welcome To React Cache Busting</h4>
+      <h4>Welcome To React Cache Busting....</h4>
       <CacheBusting />
     </div>
   );

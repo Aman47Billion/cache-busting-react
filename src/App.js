c6a5264b7f60fee26feb0739deaeaac929d44ee6
shashank47b <shashank.kanungo@47billion.com>
2021-07-16 14:48:47 +0530

    ek baar check

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import CacheBusting from "./Components/CacheBusting";
+import packageJson from "../package.json";
 
 function App() {
   return (
     <div className="App">
       <h4>Welcome To React Cache Busting....</h4>
-      <h4>47 Billion Front End</h4>
+      <h4>47 Billion v-{packageJson.version}</h4>
       <CacheBusting />
     </div>
   );
